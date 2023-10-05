ed7a74146facbd5ec10facf2c1df95658c1b607a
footballjones <footballjones308@gmail.com>
2023-10-05 11:12:26 -0400

    updated game

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Hangman
 
-This is a 1 player hangman game in which the player tries to guess the letters to the word.
+This is a 1 player hangman command line interface game in which the player tries to guess the letters to the word. There are multiple word groups the game will choose from as well as an option for the player to choose a difficulty level. 
+
+## To Play the Game
+In order to play the game you will need to clone the repository to your local machine and navigate inside the Hangman repository. When inside the repository enter in the command line **_python hangman.py_** from here just follow the instructions within the game.
--- hangman.py
+++ hangman.py
@@ -34,13 +34,29 @@ HANGMAN_PICS = ['''
   O   |
  /|\  |
  / \  |
+     ===''', '''
+  +---+
+ [O   |
+ /|\  |
+ / \  |
+     ===''', '''
+  +---+
+ [O]  |
+ /|\  |
+ / \  |
      ===''']
 
-words = 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+words ={
+    "Colors" : "red orange yellow green blue indigo violet white black pink purple".split(),
+    "Shapes" : "square triangle rectangle circle oval trapezoid hexagon octagon pentagon septagon".split(),
+    "Fruits" : "apple orange lemon lime pear watermelon grape grapefruit cherry banana cantaloupe mango strawberry".split(),
+    "Animals" : 'ant baboon badger bat bear beaver camel cat clam cobra cougar coyote crow deer dog donkey duck eagle ferret fox frog goat goose hawk lion lizard llama mole monkey mouse mule newt otter owl panda parrot pigeon python rabbit ram rat raven rhino salmon seal shark sheep skunk sloth snake spider stork swan tiger toad trout turkey turtle weasel whale wolf wombat zebra'.split()
+}
 
-def getRandomWord(wordList):
-    wordIndex = random.randint(0, len(wordList) - 1)
-    return wordList[wordIndex]
+def getRandomWord(wordDict):
+    wordKey = random.choice(list(wordDict.keys()))
+    wordIndex = random.randint(0, len(wordDict[wordKey]) - 1)
+    return [wordDict[wordKey][wordIndex], wordKey]
 
 def displayBoard(missedLetters, correctLetters, secretWord):
     print(HANGMAN_PICS[len(missedLetters)])
@@ -80,12 +96,27 @@ def playAgain():
     return input().lower().startswith("y")
 
 print('H A N G M A N')
+
+difficulty = "X"
+while difficulty not in ["E", "M", "H"]:
+    print("Enter difficulty: E - Easy, M - Medium, H - Hard")
+    difficulty = input().upper()
+if difficulty == "M":
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+if difficulty == "H":
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+    del HANGMAN_PICS[5]
+    del HANGMAN_PICS[3]
+
 missedLetters = ""
 correctLetters = ""
-secretWord = getRandomWord(words)
+secretWord, secretSet = getRandomWord(words)
 gameIsDone = False
 
 while True:
+    print("The secret word is in the set: " + secretSet)
     displayBoard(missedLetters, correctLetters, secretWord)
 
     guess = getGuess(missedLetters + correctLetters)
@@ -114,6 +145,6 @@ while True:
             missedLetters = ''
             correctLetters = ''
             gameIsDone = False
-            secretWord = getRandomWord(words)
+            secretWord, secretSet = getRandomWord(words)
         else:
             break
